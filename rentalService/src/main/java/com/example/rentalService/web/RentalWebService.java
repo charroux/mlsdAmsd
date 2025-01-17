8d2322c0c14b3f3631865137858014b26bdb60cc
charroux <benoit.charroux@gmail.com>
2025-01-17 09:42:20 +0100

    request

--- rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
+++ rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
@@ -1,14 +1,30 @@
 package com.example.rentalService.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RentalWebService {
+
+    Logger logger = LoggerFactory.getLogger(RentalWebService.class);
 
     @GetMapping("/bonjour")
     public String disBonjour() {
         return "Bonjour !";
     }
+
+    @PutMapping(value = "/cars/{plateNumber}")
+    public void rent(
+            @PathVariable("plateNumber") String plateNumber,
+            @RequestParam(value="rent", required = true)boolean rent,
+            @RequestBody Dates dates){
+
+        logger.info("PlateNumber:" + plateNumber);
+        logger.info("Rent:" + rent);
+        logger.info("Dates:" + dates);
+    }
+
+
 
 }
