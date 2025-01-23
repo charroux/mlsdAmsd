8c24d3d9fb35c0c3cef3d0c882772e66b01df044
charroux <benoit.charroux@gmail.com>
2025-01-23 09:20:44 +0100

    put

--- rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
+++ rentalService/src/main/java/com/example/rentalService/web/RentalWebService.java
@@ -1,13 +1,22 @@
 package com.example.rentalService.web;
 
+import com.example.rentalService.data.Car;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class RentalWebService {
 
     Logger logger = LoggerFactory.getLogger(RentalWebService.class);
+    List<Car> cars = new ArrayList<Car>();
+
+    public RentalWebService(){
+        cars.add(new Car("AA11BB", "Ferrari", 1000));
+    }
 
     @GetMapping("/bonjour")
     public String disBonjour() {
@@ -23,6 +32,16 @@ public class RentalWebService {
         logger.info("PlateNumber:" + plateNumber);
         logger.info("Rent:" + rent);
         logger.info("Dates:" + dates);
+    }
+
+    @GetMapping("/cars")
+    public List<Car> getAllCars(){
+        return cars;
+    }
+
+    @GetMapping("/cars/{plateNumber}")
+    public Car getACar(@PathVariable("plateNumber") String plateNumber) throws CarNotFoundException {
+        return cars.stream().filter(car -> car.getPlateNumber().equals(plateNumber)).findFirst().orElseThrow(() -> new CarNotFoundException(plateNumber));
     }
 
 
